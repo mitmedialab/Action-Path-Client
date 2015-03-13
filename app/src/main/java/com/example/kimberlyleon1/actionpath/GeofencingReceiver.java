2306a24f5c184399361c64a3c3e464033abda95a
kimleon <kimleon@mit.edu>
2015-03-13 15:30:21 -0400

    notification instead of new window

--- app/src/main/java/com/example/kimberlyleon1/actionpath/GeofencingReceiver.java
+++ app/src/main/java/com/example/kimberlyleon1/actionpath/GeofencingReceiver.java
@@ -1,11 +1,15 @@
 package com.example.kimberlyleon1.actionpath;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.Notification.Builder;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
-
 
 
 public class GeofencingReceiver extends ReceiveGeofenceTransitionIntentService {
@@ -13,7 +17,9 @@ public class GeofencingReceiver extends ReceiveGeofenceTransitionIntentService {
     protected void onEnteredGeofences(String[] strings) {
         Log.d(GeofencingReceiver.class.getName(), "onEnter");
 
-        //have pop-up on screen with specified location
+        sendNotification(strings[0]);
+
+
 
 
         //change so it takes in a list of strings
@@ -23,7 +29,7 @@ public class GeofencingReceiver extends ReceiveGeofenceTransitionIntentService {
         //alert[2] is Name of alert
         //alert[3] is a short description of the alert
         //alert[4] is a picture url??
-        openAlert(strings);
+//        openAlert(strings);
     }
 
     private void openAlert(String[] alert) {
@@ -67,5 +73,89 @@ public class GeofencingReceiver extends ReceiveGeofenceTransitionIntentService {
     protected void onError(int i) {
         Log.e(GeofencingReceiver.class.getName(), "Error: " + i);
         }
+
+
+//    private void sendNotifications(String transitionType, String[] ids){
+//
+//        //retrieve the relevant survey keys and print them
+//        Context ctx = this.getApplicationContext();
+//        SurveyGeofenceStore mPrefs = new SurveyGeofenceStore(ctx);
+//        ArrayList<String> surveyKeys = mPrefs.getUniqueSurveyKeys(ids);
+//        ArrayList<String> y = mPrefs.getGeofenceStoreKeys();
+//        Log.i("NotificationContext", ctx.toString());
+//        for (String surveyKey : surveyKeys) {
+//            sendNotification(surveyKey);
+//        }
+//    }
+
+    /**
+     * Posts a notification in the notification bar when a transition is detected.
+     * If the user clicks the notification, control goes to the main Activity.
+     * @param transitionType The type of transition that occurred.
+     * For now, ActionPath only handles enter transitionTypes
+     *
+     */
+    private void sendNotification(String surveyKey) {
+
+        Log.d("sendNotification","sending notification build thing in ReceiveTransitionsIntentService");
+        Log.i("sendNotification", surveyKey);
+
+        //surveyKey="Chuckie Harris Park";
+
+        // create "surveyIntent" to be triggered when user clicks on notification
+        PendingIntent pi = getPendingIntent(surveyKey);
+
+        // create the notification
+        Builder notificationBuilder = new Notification.Builder(this);
+        notificationBuilder.setContentTitle("Action: " + surveyKey)
+                //notificationBuilder.setContentTitle("ActionPath " + transitionType + " " + TextUtils.join(GeofenceUtils.GEOFENCE_ID_DELIMITER,ids))
+                // Notification title
+                // not sure how to make this appear, or where it does appear
+                //.setContentText("You have " + transitionType + " " + ids.length + "ActionPaths")
+                // you can put subject line.
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("My notification")
+                .setContentText("it works!");
+                        // Set your notification icon here.
+
+                        //TODO: ADD THESE BACK IN WHEN NEEDED
+//                .addAction(R.drawable.ic_notification_overlay, "Take Action", pi);
+    	/*.addAction(
+    			R.drawable.ic_stat_snooze,
+    			"Snooze", pi); // TODO: Make this an actual snooze button*/
+
+        //notificationBuilder.setContentIntent(pi);
+
+
+        // Now create the Big picture notification.
+        Notification notification = new Notification.BigTextStyle(notificationBuilder)
+                .bigText("Take the survey!").build();
+        //	Notification notification = new Notification.BigPictureStyle(notificationBuilder).build();
+    	/*.bigPicture(
+    			BitmapFactory.decodeResource(getResources(),
+    					R.drawable.ic_notification_placeholder)).build();*/
+        // Put the auto cancel notification flag
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        NotificationManager notificationManager = getNotificationManager();
+        notificationManager.notify(0, notification);
+
+        // TODO: Create a way to clear the notification once it has been clicked
+
+    }
+
+
+    //creates a PendingIntent for bigPicture notifications
+    public PendingIntent getPendingIntent(String surveyKey) {
+        Log.v("INTENT","returning an intent for SurveyActivity.class");
+
+        Intent surveyIntent = new Intent(this, Activity.class)
+                .putExtra("surveyKey", surveyKey);
+
+        return PendingIntent.getActivity(this, 0, surveyIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
+    public NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
 
 }
