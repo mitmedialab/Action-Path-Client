1054c00f24eca8faf9fb1393c828f6dbd7e3592e
kimleon <kimleon@mit.edu>
2015-04-03 16:50:25 -0400

    creating geofences separate method

--- app/src/main/java/com/example/kimberlyleon1/actionpath/AlertTest.java
+++ app/src/main/java/com/example/kimberlyleon1/actionpath/AlertTest.java
@@ -11,8 +11,6 @@ import android.widget.TextView;
 import com.google.android.gms.location.Geofence;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -25,27 +23,18 @@ import java.util.List;
 public class AlertTest extends Activity {
 
     private TextView filler1;
+    final float rad = 500;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
-        final float rad = 500;
-
-        List<Geofence> cambridge = new ArrayList<Geofence>();
         final double Cambridge_lat = 42.359254;
         final double Cambridge_long = -71.093667;
         final float Cambridge_rad = 2001;
-        Geofence.Builder builder_test = new Geofence.Builder();
-        builder_test.setRequestId("1234");
-        builder_test.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
-        builder_test.setCircularRegion(Cambridge_lat, Cambridge_long, Cambridge_rad);
-        builder_test.setExpirationDuration(5000);
-
-        GeofencingRegisterer registerCambridge = new GeofencingRegisterer(this);
-        cambridge.add(builder_test.build());
-        registerCambridge.registerGeofences(cambridge);
+        String id = "1234";
+        buildGeofence(Cambridge_lat, Cambridge_long, Cambridge_rad, id);
 
         filler1 = (TextView) findViewById(R.id.filler1);
         filler1.setOnClickListener(new OnClickListener() {
@@ -69,11 +58,7 @@ public class AlertTest extends Activity {
                     while((line = reader.readLine()) != null) {
                         result.append(line);
                     }
-//                    System.out.println(result.toString());
-
-
-
-
+                    parseResult(result.toString());
                     Log.e("GAH", "url success: "+ result.toString());
                 } catch (Exception ex) {
 
@@ -84,62 +69,27 @@ public class AlertTest extends Activity {
         });
         thread.start();
 
+    }
 
+    // parse result from server and send info to create geofences
+    public void parseResult(String result){
 
-//        String readJSON = HTTPRequests.getJSON("https://api.dev.actionpath.org/places/9841/issues/");
-//        try{
-//
-//            JSONArray array = new JSONArray(readJSON);
-//            for (int i = 0; i < array.length(); i++) {
-//                JSONObject issue = array.getJSONObject(i);
-//                String issue_status = issue.getString("status");
-//                double issue_lat = issue.getInt("lat");
-//                double issue_long = issue.getInt("lng");
-//                String issue_id = issue.getString("id");
-//                String issue_summary = issue.getString("summary");
-//
-//                Geofence.Builder builder123 = new Geofence.Builder();
-//                builder123.setRequestId(issue_id);
-//                builder123.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
-//                builder123.setCircularRegion(issue_lat, issue_long, rad);
-//                builder123.setExpirationDuration(5000);
-//            }
-//
-//
-//            //info for each issue:
-//            //   "status"
-//            //   "summary"
-//            //   "description"
-//            //  "lat"
-//            //  "lng"
-//            //     "address"
-//            //  "image_full"
-//            // "created_at",  null: false
-//            // "updated_at",  null: false
-//            //  "place_id"
-//
-//
-//        } catch(Exception e){e.printStackTrace();}
-//        finally{System.out.println("Success");}
-//
-//
     }
 
 
-    public String readFully(InputStream inputStream, String encoding)
-            throws IOException {
-        return new String(readFully(inputStream), encoding);
-    }
+    // creates a geofence at given location of given radius
+    // TODO: keep track of each geofence's summary, address, etc.
+    public void buildGeofence(double latitude, double longitude, float radius, String id){
+        List<Geofence> new_geo = new ArrayList<>();
+        Geofence.Builder builder_test = new Geofence.Builder();
+        builder_test.setRequestId(id);
+        builder_test.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
+        builder_test.setCircularRegion(latitude, longitude, radius);
+        builder_test.setExpirationDuration(5000);
 
-    private byte[] readFully(InputStream inputStream)
-            throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int length = 0;
-        while ((length = inputStream.read(buffer)) != -1) {
-            baos.write(buffer, 0, length);
-        }
-        return baos.toByteArray();
+        GeofencingRegisterer registerCambridge = new GeofencingRegisterer(this);
+        new_geo.add(builder_test.build());
+        registerCambridge.registerGeofences(new_geo);
     }
 
 
