a67f5df5de02023f421bea4597f728bc7f1119df
erhardt <erhardt@gmail.com>
2015-08-06 15:13:46 -0400

    Fixed various warnings

--- app/src/main/java/org/actionpath/geofencing/GeofencingRegistererCallbacks.java
+++ app/src/main/java/org/actionpath/geofencing/GeofencingRegistererCallbacks.java
@@ -5,9 +5,9 @@ import com.google.android.gms.common.ConnectionResult;
 
 
 public interface GeofencingRegistererCallbacks {
-    public void onApiClientConnected();
-    public void onApiClientSuspended();
-    public void onApiClientConnectionFailed(ConnectionResult connectionResult);
+    void onApiClientConnected();
+    void onApiClientSuspended();
+    void onApiClientConnectionFailed(ConnectionResult connectionResult);
 
-    public void onGeofencesRegisteredSuccessful();
+    void onGeofencesRegisteredSuccessful();
 }
