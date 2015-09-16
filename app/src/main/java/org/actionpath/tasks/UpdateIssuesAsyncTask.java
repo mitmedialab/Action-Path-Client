48de77de3287f2927eb467a38fc50a2338b613bb
Rahul Bhargava <rahulb@media.mit.edu>
2015-09-16 13:42:46 -0400

    move updating issues into an independant task sowe can all it form any place

---
+++ app/src/main/java/org/actionpath/tasks/UpdateIssuesAsyncTask.java
@@ -0,0 +1,177 @@
+package org.actionpath.tasks;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+
+import org.actionpath.db.issues.Issue;
+import org.actionpath.db.issues.IssuesDataSource;
+import org.actionpath.db.logs.LogMsg;
+import org.actionpath.db.logs.LogsDataSource;
+import org.actionpath.geofencing.GeofencingRegisterer;
+import org.actionpath.geofencing.GeofencingRegistrationListener;
+import org.actionpath.geofencing.GeofencingRemovalListener;
+import org.actionpath.geofencing.GeofencingRemover;
+import org.actionpath.util.ActionPathServer;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Call this task to update the issues for the place selected.
+ */
+public class UpdateIssuesAsyncTask extends AsyncTask<Object, Void, Object> implements
+        GeofencingRegistrationListener, GeofencingRemovalListener {
+
+    public final String TAG = this.getClass().getName();
+
+    private OnIssuesUpdatedListener listener;
+
+    public UpdateIssuesAsyncTask(OnIssuesUpdatedListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        Log.d(TAG, "Loading new issues for place " + listener.getPlaceId());
+        ArrayList<Issue> newIssues = null;
+        try {
+            newIssues = ActionPathServer.getLatestIssues(listener.getPlaceId());
+            IssuesDataSource dataSource = IssuesDataSource.getInstance();
+            for (Issue i : newIssues) {
+                if (dataSource.issueExists(i.getId())) {
+                    Issue existingIssue = dataSource.getIssue(i.getId());
+                    if (existingIssue.isFollowed() && existingIssue.hasStatus() && !existingIssue.getStatus().equals(i.getStatus())) {
+                        listener.onFollowedIssueStatusChanged(i.getId(), existingIssue.getStatus(), i.getStatus());
+                    }
+                    dataSource.updateIssue(i, true);
+                } else {
+                    LogsDataSource.getInstance(listener.getContext()).insert(listener.getContext(),
+                            LogMsg.ACTION_CREATED_ISSUE, null);
+                    dataSource.insertIssue(i);
+                }
+            }
+            Log.d(TAG, "Pulled " + newIssues.size() + " issues from the server for place " + listener.getPlaceId());
+            removeExistingGeofencesExcept(newIssues);
+            buildGeofences();
+            return newIssues;
+        } catch (IOException ex) {
+            Log.e(TAG, "Failed to pull new issues for " + listener.getPlaceId() + " | " + ex.toString());
+        } catch (JSONException ex) {
+            Log.e(TAG, "Failed to parse issues json from server for " + listener.getPlaceId() + " | " + ex);
+        }
+        return null;
+    }
+
+    private void removeExistingGeofencesExcept(ArrayList<Issue> issuesNeedingGeofences) {
+        ArrayList<Integer> issueIdsToKeep = new ArrayList<>();
+        for(Issue issue:issuesNeedingGeofences){
+            issueIdsToKeep.add(issue.getId());
+        }
+        Log.d(TAG, "Removing existing geofences");
+        IssuesDataSource issuesDataSource = IssuesDataSource.getInstance(listener.getContext());
+        Cursor cursor  = issuesDataSource.getIssuesWithGeofences();
+        while(!cursor.isAfterLast()){
+            int issueId = cursor.getInt(0);
+            if(!issueIdsToKeep.contains(issueId)){
+                List<String> requestIds = new ArrayList<>();
+                requestIds.add(issueId+"");
+                GeofencingRemover remover = new GeofencingRemover(
+                        listener.getContext(),requestIds,this);
+                remover.sendRequest();
+            }
+            cursor.moveToNext();
+        }
+        cursor.close();
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+        if (o == null) {
+            listener.onIssueUpdateFailed();
+        } else {
+            ArrayList<Issue> newIssues = (ArrayList<Issue>) o;
+            Log.d(TAG, "Got " + newIssues.size() + " issues from server");
+            listener.onIssuesUpdateSucceeded(newIssues.size());
+        }
+    }
+
+
+    @Override
+    public void onGeofenceRegistrationSuccess(List data){
+        List<Geofence> newGeofences = (List<Geofence>) data;
+        for(Geofence geofence: newGeofences){
+            Log.d(TAG, "Created geofence for "+geofence.getRequestId());
+            IssuesDataSource.getInstance().updateIssueGeofenceCreated(
+                    Integer.parseInt(geofence.getRequestId()), true);
+        }
+    }
+    @Override
+    public void onGeofenceRegistrationFailure(com.google.android.gms.common.api.Status status){
+        Log.d(TAG, "Got geofence registration failure - " + status);
+    }
+
+    @Override
+    public void onGeofenceRemovalSuccess(List data){
+        List<String> requestIds = (List<String>) data;
+        for(String requestId: requestIds){
+            Log.d(TAG, "Removed geofence for "+requestId);
+            IssuesDataSource.getInstance().updateIssueGeofenceCreated(
+                    Integer.parseInt(requestId), false);
+        }
+    }
+
+    @Override
+    public void onGeofenceRemovalFailure(com.google.android.gms.common.api.Status status){
+        Log.d(TAG, "Got geofence registration failure - " + status);
+    }
+
+    /**
+     * Build geofences for all non-geofenced issues in the database
+     * TODO: consider filtering for closed issues, and remember we can only do 100 total
+     */
+    private void buildGeofences(){
+        Log.d(TAG, "Building geofences");
+        IssuesDataSource issuesDataSource = IssuesDataSource.getInstance(listener.getContext());
+        Cursor cursor = issuesDataSource.getIssuesToGeofenceCursor(listener.getPlaceId());
+        while (!cursor.isAfterLast()) {
+            int issueId = cursor.getInt(0);
+            Issue issue = issuesDataSource.getIssue(issueId);
+            if(issue.getRadius()==0){
+                Log.e(TAG,"  skipping geofence for issue "+issueId+" due to radius of 0");
+            } else {
+                buildGeofence(issueId, cursor.getDouble(1), cursor.getDouble(2), issue.getRadius());
+                //logMsg(issueId, LogMsg.ACTION_ADDED_GEOFENCE);
+            }
+            cursor.moveToNext();
+        }
+        cursor.close();
+    }
+
+    private void buildGeofence(int issueId, double latitude, double longitude, float radius){
+        List<Geofence> newGeofences = new ArrayList<>();
+        Geofence.Builder geofenceBuilder = new Geofence.Builder();
+        geofenceBuilder.setRequestId((Integer.valueOf(issueId)).toString());
+        geofenceBuilder.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
+        geofenceBuilder.setCircularRegion(latitude, longitude, radius);
+        geofenceBuilder.setExpirationDuration(Geofence.NEVER_EXPIRE);
+        newGeofences.add(geofenceBuilder.build());
+        GeofencingRegisterer registerer= new GeofencingRegisterer(
+                listener.getContext().getApplicationContext(), newGeofences,this);
+        registerer.sendRequest();
+    }
+
+    public interface OnIssuesUpdatedListener {
+        Context getContext();
+        int getPlaceId();
+        void onIssuesUpdateSucceeded(int newIssueCount);
+        void onIssueUpdateFailed();
+        void onFollowedIssueStatusChanged(int issueId, String oldStatus, String newStatus);
+    }
+}
--- app/src/main/java/org/actionpath/ui/UpdateIssuesFragment.java
+++ app/src/main/java/org/actionpath/ui/UpdateIssuesFragment.java
@@ -1,8 +1,6 @@
 package org.actionpath.ui;
 
 import android.app.Activity;
-import android.content.Context;
-import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,46 +9,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.Geofence;
-
 import org.actionpath.R;
-import org.actionpath.geofencing.GeofencingRegisterer;
-import org.actionpath.geofencing.GeofencingRegistrationListener;
-import org.actionpath.geofencing.GeofencingRemovalListener;
-import org.actionpath.geofencing.GeofencingRemover;
-import org.actionpath.db.issues.Issue;
-import org.actionpath.db.issues.IssuesDataSource;
-import org.actionpath.db.logs.LogMsg;
-import org.actionpath.db.logs.LogsDataSource;
-import org.actionpath.util.ActionPathServer;
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.actionpath.tasks.UpdateIssuesAsyncTask;
 
 /**
  * A fragment representing a list of followed issues.
  * <p/>
- * Activities containing this fragment MUST implement the {@link OnIssuesUpdatedListener}
+ * Activities containing this fragment MUST implement the {@link UpdateIssuesAsyncTask.OnIssuesUpdatedListener}
  * interface.
  */
-public class UpdateIssuesFragment extends Fragment implements
-        GeofencingRegistrationListener, GeofencingRemovalListener {
+public class UpdateIssuesFragment extends Fragment {
 
     private static String TAG = UpdateIssuesFragment.class.getName();
 
-    private static final String ARG_PLACE_ID = "ARG_PLACE_ID";
-
-    private OnIssuesUpdatedListener listener;
-    private int placeId;
-    private AsyncTask fetchingTask;
+    private UpdateIssuesAsyncTask.OnIssuesUpdatedListener listener;
+    private AsyncTask updateIssuesTask;
 
     public static UpdateIssuesFragment newInstance(int placeId) {
         UpdateIssuesFragment fragment = new UpdateIssuesFragment();
         Bundle args = new Bundle();
-        args.putInt(ARG_PLACE_ID, placeId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -66,7 +43,6 @@ public class UpdateIssuesFragment extends Fragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            placeId = getArguments().getInt(ARG_PLACE_ID);
         }
     }
 
@@ -84,7 +60,7 @@ public class UpdateIssuesFragment extends Fragment implements
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            listener = (OnIssuesUpdatedListener) activity;
+            listener = (UpdateIssuesAsyncTask.OnIssuesUpdatedListener) activity;
             startFetchingTask();
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString() + " must implement OnIssuesUpdatedListener");
@@ -92,152 +68,17 @@ public class UpdateIssuesFragment extends Fragment implements
     }
 
     private void startFetchingTask(){
-        final Context context = getActivity().getApplicationContext();
-        fetchingTask = new AsyncTask<Object, Void, Object>() {
-            @Override
-            protected Object doInBackground(Object[] params) {
-                Log.d(TAG, "Loading new issues for place "+placeId);
-                ArrayList<Issue> newIssues = null;
-                try {
-                    newIssues = ActionPathServer.getLatestIssues(placeId);
-                    IssuesDataSource dataSource = IssuesDataSource.getInstance();
-                    for(Issue i:newIssues){
-                        if(dataSource.issueExists(i.getId())){
-                            Issue existingIssue = dataSource.getIssue(i.getId());
-                            if(existingIssue.isFollowed() && existingIssue.hasStatus() && !existingIssue.getStatus().equals(i.getStatus())){
-                                listener.onFollowedIssueStatusChanged(i.getId(), existingIssue.getStatus(), i.getStatus());
-                            }
-                            dataSource.updateIssue(i,true);
-                        } else {
-                            LogsDataSource.getInstance(context).insert(context, LogMsg.ACTION_CREATED_ISSUE, null);
-                            dataSource.insertIssue(i);
-                        }
-                    }
-                    Log.d(TAG, "Pulled " + newIssues.size() + " issues from the server for place " + placeId);
-                    removeExistingGeofencesExcept(newIssues);
-                    buildGeofences();
-                    return newIssues;
-                } catch (IOException ex){
-                    Log.e(TAG, "Failed to pull new issues for " + placeId + " | " + ex.toString());
-                } catch (JSONException ex){
-                    Log.e(TAG, "Failed to parse issues json from server for "+placeId+" | "+ex);
-                }
-                return null;
-            }
-            @Override
-            protected void onPostExecute(Object o) {
-                super.onPostExecute(o);
-                if(o==null){
-                    listener.onIssueUpdateFailed();
-                } else {
-                    ArrayList<Issue> newIssues = (ArrayList<Issue>) o;
-                    Log.d(TAG, "Got " + newIssues.size() + " issues from server");
-                    listener.onIssuesUpdateSucceeded(newIssues.size());
-                }
-            }
-        };
-        fetchingTask.execute();
+        updateIssuesTask = new UpdateIssuesAsyncTask(listener);
+        updateIssuesTask.execute();
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        if (this.fetchingTask != null) {
-            this.fetchingTask.cancel(true);
+        if (this.updateIssuesTask != null) {
+            this.updateIssuesTask.cancel(true);
         }
         listener = null;
-    }
-
-    public interface OnIssuesUpdatedListener {
-        void onIssuesUpdateSucceeded(int newIssueCount);
-        void onIssueUpdateFailed();
-        void onFollowedIssueStatusChanged(int issueId, String oldStatus, String newStatus);
-    }
-
-    private void removeExistingGeofencesExcept(ArrayList<Issue> issuesNeedingGeofences) {
-        ArrayList<Integer> issueIdsToKeep = new ArrayList<>();
-        for(Issue issue:issuesNeedingGeofences){
-            issueIdsToKeep.add(issue.getId());
-        }
-        Log.d(TAG, "Removing existing geofences");
-        IssuesDataSource issuesDataSource = IssuesDataSource.getInstance(this.getActivity());
-        Cursor cursor  = issuesDataSource.getIssuesWithGeofences();
-        while(!cursor.isAfterLast()){
-            int issueId = cursor.getInt(0);
-            if(!issueIdsToKeep.contains(issueId)){
-                List<String> requestIds = new ArrayList<>();
-                requestIds.add(issueId+"");
-                GeofencingRemover remover = new GeofencingRemover(
-                        this.getActivity().getApplicationContext(),requestIds,this);
-                remover.sendRequest();
-            }
-            cursor.moveToNext();
-        }
-        cursor.close();
-    }
-
-    /**
-     * Build geofences for all non-geofenced issues in the database
-     * TODO: consider filtering for closed issues, and remember we can only do 100 total
-     */
-    private void buildGeofences(){
-        Log.d(TAG, "Building geofences");
-        IssuesDataSource issuesDataSource = IssuesDataSource.getInstance(this.getActivity());
-        Cursor cursor = issuesDataSource.getIssuesToGeofenceCursor(placeId);
-        while (!cursor.isAfterLast()) {
-            int issueId = cursor.getInt(0);
-            Issue issue = issuesDataSource.getIssue(issueId);
-            if(issue.getRadius()==0){
-                Log.e(TAG,"  skipping geofence for issue "+issueId+" due to radius of 0");
-            } else {
-                buildGeofence(issueId, cursor.getDouble(1), cursor.getDouble(2), issue.getRadius());
-                //logMsg(issueId, LogMsg.ACTION_ADDED_GEOFENCE);
-            }
-            cursor.moveToNext();
-        }
-        cursor.close();
-    }
-
-    private void buildGeofence(int issueId, double latitude, double longitude, float radius){
-        List<Geofence> newGeofences = new ArrayList<>();
-        Geofence.Builder geofenceBuilder = new Geofence.Builder();
-        geofenceBuilder.setRequestId((Integer.valueOf(issueId)).toString());
-        geofenceBuilder.setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
-        geofenceBuilder.setCircularRegion(latitude, longitude, radius);
-        geofenceBuilder.setExpirationDuration(Geofence.NEVER_EXPIRE);
-        newGeofences.add(geofenceBuilder.build());
-        GeofencingRegisterer registerer= new GeofencingRegisterer(
-                this.getActivity().getApplicationContext(), newGeofences,this);
-        registerer.sendRequest();
-    }
-
-    @Override
-    public void onGeofenceRegistrationSuccess(List data){
-        List<Geofence> newGeofences = (List<Geofence>) data;
-        for(Geofence geofence: newGeofences){
-            Log.d(TAG, "Created geofence for "+geofence.getRequestId());
-            IssuesDataSource.getInstance().updateIssueGeofenceCreated(
-                    Integer.parseInt(geofence.getRequestId()), true);
-        }
-    }
-    @Override
-    public void onGeofenceRegistrationFailure(Status status){
-        Log.d(TAG,"Got geofence registration failure - "+status);
-    }
-
-    @Override
-    public void onGeofenceRemovalSuccess(List data){
-        List<String> requestIds = (List<String>) data;
-        for(String requestId: requestIds){
-            Log.d(TAG, "Removed geofence for "+requestId);
-            IssuesDataSource.getInstance().updateIssueGeofenceCreated(
-                    Integer.parseInt(requestId), false);
-        }
-    }
-
-    @Override
-    public void onGeofenceRemovalFailure(Status status){
-        Log.d(TAG,"Got geofence registration failure - "+status);
     }
 
 }
